34e4bdcdf3f0e54e0726318b7bffa8fed46996dc
Imam Satria <imam.satria280897@gmail.com>
2019-04-05 14:19:34 +0700

    change code structure and package

--- index.js
+++ index.js
@@ -1,104 +1,54 @@
-var http = require("http");
-var url = require("url");
-var fs  = require("fs");
-var qs = require("querystring");
-var sql = require("mssql");
-var port = process.env.PORT;
-// var port = process.env.PORT | 1000;
+var express     = require("express")
+var bodyParser  = require("body-parser")
+var sql 		= require("mssql");
+var app         = express()
 
-var server = http.createServer((request, response) => {
-    console.log(__dirname);
-    // console.log(request.method);
-    // console.log(req.url);
-    // console.log(url.parse(req.url));
+app.set("view engine", "ejs")
+app.use(bodyParser.urlencoded({extended: false}))
 
-    var pathName = url.parse(request.url).pathname
-    // console.log('pathname' + pathName);
+// var url = require("url");
+// var fs  = require("fs");
+// var qs = require("querystring");
 
-    if(request.method == "GET")
-        showPage(response, pathName);
-    else{
-        switch(pathName){
-            case "/":
-                // console.log("Processing...");   
-                parseData(request, response, function(){
-                    // console.log(request.post); 
-                    response.writeHead(200, "OK", {'Content-Type': 'text/plain'});
-                    response.end();
-                });                
-                break;
-        }
-    }
+// var port = process.env.PORT;
+var port = process.env.PORT | 1000
+
+// Create a configuration object for our Azure SQL connection parameters
+var dbConfig = {
+    server: "daftartugaswebapp.database.windows.net", // Use your SQL server name
+    database: "daftartugas", // Database to connect to
+    user: "genered", // Use your username
+    password: "Burliku1", // Use your password
+    port: 1433,
+    // Since we're on Windows Azure, we need to set the following options
+    options: {
+          encrypt: true
+      }
+   }
+
+app.get('/', function (req, res) {
+    res.render("index")
 });
 
-function parseData(request, response, callback){
-    var body = "";    
-
-    request.on('data', function (data) {
-        body += data;        
-        // console.log("Processing body...");  
-        // 1e6 === 1 * Math.pow(10, 6) === 1 * 1000000 ~~~ 1MB, too much data
-        if (body.length > 1e6) { 
-            // FLOOD ATTACK OR FAULTY CLIENT, NUKE REQUEST
-            response.writeHead(413, {'Content-Type': 'text/plain'}).end();
-            request.connection.destroy();
-        }
-
+app.post('/', function (req, res) {
+	sql.connect(dbConfig, function (err) {    
+		if (err) 
+			console.log(err);
+		else {
+			alert("Connected")
+			// create Request object
+			var request = new sql.Request()
+			
+			// query to the database and get the records
+			request.query('select * from Student', function (err, recordset) {			
+				if (err) console.log(err)
+					// send records as a response
+					res.send(recordset)				
+			});
+		}
     });
+});
 
-    request.on('end', function () {
-        request.post = qs.parse(body);      
-        // console.log("Parsing the body...");    
-        callback();        
-    });        
-}
-
-function showPage(response, pathName){
-    switch(pathName){
-        case "/create":            
-            // alert(__dirname);
-            // response.end("Tes");
-            fs.readFile(__dirname + "/views/index.html", null, (err, data) => {
-                if(err){                
-                    response.writeHead(404);
-                    response.write("Can\'t find the file.");
-                    response.end();                
-                }
-                else {
-                    response.writeHead(200, {"Content-Type" : "text/html"});
-                    response.write(data);
-                    response.end();
-                }
-            });                        
-        break;
-        default:
-            response.writeHead(404);
-            response.write("Can\'t find the page you looking for. " + __dirname + ", " + pathName);
-            response.end();
-            break;
-    }    
-}
-
-server.listen(port);
+// server.listen(port);
+app.listen(port)
 console.log("Server is running on port %d", port);
-
-// // Create a configuration object for our Azure SQL connection parameters
-// var dbConfig = {
-//  server: "daftartugaswebapp.database.windows.net", // Use your SQL server name
-//  database: "daftartugas", // Database to connect to
-//  user: "genered", // Use your username
-//  password: "Burliku1", // Use your password
-//  port: 1433,
-//  // Since we're on Windows Azure, we need to set the following options
-//  options: {
-//        encrypt: true
-//    }
-// };
-
-// sql.connect(dbConfig).then(conn => {
-//     // console.log(conn);
-//     console.log("Connected!");
-// })
-// .catch(e => {
-//     console.error('connection error', e.message, e.stack)
-// });
