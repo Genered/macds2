ff2cc96ae25e80fa8beb2c0c066c9a5f6575ee86
Imam Satria <imam.satria280897@gmail.com>
2019-04-06 13:23:21 +0700

    add date column to database

--- index.js
+++ index.js
@@ -7,29 +7,29 @@ var port = process.env.PORT
 
 // Create a configuration object for our Azure SQL connection parameters
 var dbConfig = {
-    server: "daftartugaswebapp.database.windows.net", // Use your SQL server name
-    database: "daftartugas", // Database to connect to
-    user: "genered", // Use your username
-    password: "Burliku1", // Use your password
-    port: 1433,
-    // Since we're on Windows Azure, we need to set the following options
+    server: "daftartugaswebapp.database.windows.net", 
+    database: "daftartugas", 
+    user: "genered", 
+    password: "Burliku1", 
+    port: 1433,    
     options: {
-          encrypt: true
-      }
-   }
+		encrypt: true
+    }
+}
 
 app.set("view engine", "ejs")
 app.use(bodyParser.urlencoded({extended: false}))
 
 app.get('/', function (req, res) {
-	var todos = [{"task" : "Minum", "deadline" : "13.00"},
-				 {"task" : "Makan", "deadline" : "12.00"}];   				 
+	// var todos = [{"task" : "Minum", "deadline" : "13.00"},
+	// 			 {"task" : "Makan", "deadline" : "12.00"}];   		
+			 
 	sql.connect(dbConfig, (err) => {		
 		if(err)
 			console.log(err);
 		else {
 			var request = new sql.Request();
-			request.query("select * from todos", (err, record) => {
+			request.query("select * from todo", (err, record) => {
 				if(err)
 					console.log(err);
 				else {
@@ -47,17 +47,21 @@ app.post('/', function (req, res) {
 		if(err)
 			console.log(err);
 		else {
+			var date = new Date().toISOString().split('T')[0];	
+			// console.log(date);		
 			var request = new sql.Request();
-			var query = "insert into todos (task, deadline) values (" + 
+			var query = "insert into todo (task, deadline, addedDate) values (" + 
 						"'" + req.body.task + "', " +
-						"'" + req.body.deadline + "')";
+						"'" + req.body.deadline + "', " +
+						"'" + date + "')";
+			// console.log(query);
 			request.query(query, (err, record) => {
 				if(err)
 					console.log(err);
 				else{					
 					res.redirect("/");	
 					sql.close();		
-					console.log(record.recordset);
+					// console.log(record.recordset);
 				}
 			});			
 		}					
